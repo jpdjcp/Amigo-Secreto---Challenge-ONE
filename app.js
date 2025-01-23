f026b7c835bb37e451fd1de68af612d583b4422d
Marcelo Palacios <jpdjcp@gmail.com>
2025-01-23 19:27:49 -0300

    Update app.js implement agregarAmigo

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let listaAmigos = [];
 
 function agregarAmigo() {
-    
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (esValido(nombreAmigo)) {
+        listaAmigos.push(nombreAmigo);
+    } else {
+        alert("Debe ingresar un nombre");
+    }
 }
 
 function sortearAmigo() {
