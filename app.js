f43ec26f9ea2571c1ae154c392864f29167ba54c
Marcelo Palacios <jpdjcp@gmail.com>
2025-01-23 19:43:04 -0300

    Update app.js implement sortearAmigo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let listaAmigos = [];
 
 function agregarAmigo() {
     let nombreAmigo = document.getElementById("amigo").value;
-    
+
     if (esValido(nombreAmigo)) {
         listaAmigos.push(nombreAmigo);
     } else {
@@ -11,7 +11,11 @@ function agregarAmigo() {
 }
 
 function sortearAmigo() {
-
+    let indiceRandom = generarIndiceRandom(); // implementar
+    console.log(listaAmigos[indiceRandom]); // borrar
+    let nombreSorteado = listaAmigos[indiceRandom];
+    console.log(listaAmigos[indiceRandom]); // borrar
+    asignarTextoElementoHTML("", nombreSorteado); // implementar
 }
 
 function esValido(nombre) {
