d9ffbf1fe301d8f630a39152abd43505ac5589fe
Marcelo Palacios <jpdjcp@gmail.com>
2025-01-23 19:10:08 -0300

    Update app.js agregarAmigo & sortearAmigo

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo() {
+
+}
+
+function sortearAmigo() {
+
+}
